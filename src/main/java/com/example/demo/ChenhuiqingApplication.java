dee3a29f0ccc8a74c416a57362bd774038518d5c
chq-raphael <184977514@qq.com>
2018-01-15 10:46:34 +0800

    想

--- src/main/java/com/example/demo/ChenhuiqingApplication.java
+++ src/main/java/com/example/demo/ChenhuiqingApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //test
 //nihao
 //222222
+//a
 public class ChenhuiqingApplication {
 
 	public static void main(String[] args) {
